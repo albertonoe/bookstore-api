39a3376e13c42636a90d8882e49c60c3ff53aa07
albertonoe <albertoc660@gmail.com>
2021-03-19 20:08:44 +0100

    commit inicial

---
+++ src/test/java/com/alberto/bookstore/BookstoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alberto.bookstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookstoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
